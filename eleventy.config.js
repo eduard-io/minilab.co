cc24cc6427822c467fefa01ae762d2250a7214e5
Eduard Giménez <eduard@eduard.io>
2025-03-03 23:27:03 +0100

    [Feat] Implement inner pages

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,3 +1,4 @@
 export default function(eleventyConfig) {
   eleventyConfig.addWatchTarget("./assets/"); // Watch /assets for changes
+  eleventyConfig.addPassthroughCopy("assets/images"); // Ensure images are copied to _site
 };
