cab1533e3874ac0f28356cd232f4d2e3be62a9e2
Eduard Giménez <eduard@eduard.io>
2025-10-15 22:12:51 +0200

    chore: clean-up and simplification

--- README.md
+++ README.md
@@ -1,12 +1,13 @@
 minilab.co
 
-Designed in Figma, built in HTML and CSS in Visual Studio Code & Cursor by Eduard Giménez.
+Designed in Figma, built in HTML and CSS in Nova by Eduard Giménez.
 
 [![Netlify Status](https://api.netlify.com/api/v1/badges/80006761-2c99-4ab2-8240-4a2f7daa8b53/deploy-status)](https://app.netlify.com/sites/minilab/deploys)
 
 ## Content License
 
 All content on this site (notes, articles, and blog posts) is licensed under
-[CC BY 4.0](https://creativecommons.org/licenses/by/4.0/) unless otherwise noted.
+[CC BY 4.0](https://creativecommons.org/licenses/by/4.0/) unless otherwise
+noted.
 
 © 2025 Eduard Giménez
--- eleventy.config.js
+++ eleventy.config.js
@@ -1,4 +1,21 @@
 export default function (eleventyConfig) {
-  eleventyConfig.addWatchTarget('./assets/'); // Watch /assets for changes
-  eleventyConfig.addPassthroughCopy('assets/images'); // Ensure images are copied to _site
+  eleventyConfig.addWatchTarget("public/"); // Watch /assets for changes
+  eleventyConfig.addPassthroughCopy({ public: "/" });
+
+  // Pass environment variables to templates
+  eleventyConfig.addGlobalData("env", {
+    CONTEXT: process.env.CONTEXT || "development",
+  });
+
+  return {
+    dir: {
+      input: "src",
+      includes: "_includes",
+      data: "_data",
+      output: "_site",
+    },
+    markdownTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
+    templateFormats: ["njk", "md", "html"],
+  };
 }
