bd46fab0de2d286f79f076e186d68545c7b07fb4
Eduard Giménez <eduard@eduard.io>
2025-10-16 16:36:38 +0200

    feat(WIP): updated styles

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,21 +1,21 @@
 export default function (eleventyConfig) {
-  eleventyConfig.addWatchTarget("public/"); // Watch /assets for changes
-  eleventyConfig.addPassthroughCopy({ public: "/" });
+  eleventyConfig.addWatchTarget('public/'); // Watch /assets for changes
+  eleventyConfig.addPassthroughCopy({ public: '/' });
 
   // Pass environment variables to templates
-  eleventyConfig.addGlobalData("env", {
-    CONTEXT: process.env.CONTEXT || "development",
+  eleventyConfig.addGlobalData('env', {
+    CONTEXT: process.env.CONTEXT || 'development',
   });
 
   return {
     dir: {
-      input: "src",
-      includes: "_includes",
-      data: "_data",
-      output: "_site",
+      input: 'src',
+      includes: '_includes',
+      data: '_data',
+      output: '_site',
     },
-    markdownTemplateEngine: "njk",
-    htmlTemplateEngine: "njk",
-    templateFormats: ["njk", "md", "html"],
+    markdownTemplateEngine: 'njk',
+    htmlTemplateEngine: 'njk',
+    templateFormats: ['njk', 'md', 'html'],
   };
 }
---
+++ src/index.md
@@ -0,0 +1,25 @@
+---
+title: minilab.co
+layout: layouts/default
+---
+
+# Minilab
+
+_A quiet place_
+
+A small site to share ideas, notes and experiments by [Eduard Giménez](https://eduard.io). Searching for the DIY spirit in a dead internet.
+
+The minilab is always under construction.
+
+--- 
+
+## Colophon
+
+The minilab is built by hand on [Nova](https://nova.app), using [new.css](https://newcss.net/usage), [Inter](https://rsms.me/inter/) and, [11ty](https://www.11ty.dev) to build the site.
+
+Hosted on [Netlify](https://www.netlify.com).
+
+Last updated on October 2025.
+
+### And three
+This is just for testing
