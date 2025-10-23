1a2932c5c89fb2907056b601e4c847da3b944706
Eduard Giménez <eduard@eduard.io>
2025-10-23 23:39:43 +0200

    feat: better header/breadcrumbs

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,21 +1,28 @@
-export default function (eleventyConfig) {
-  eleventyConfig.addWatchTarget('public/'); // Watch /assets for changes
-  eleventyConfig.addPassthroughCopy({ public: '/' });
+export default function(eleventyConfig) {
+	eleventyConfig.addFilter('split', (str, delimiter = '/') => {
+		if (!str) return [];
+		return String(str)
+			.split(delimiter)
+			.filter(Boolean);
+	});
 
-  // Pass environment variables to templates
-  eleventyConfig.addGlobalData('env', {
-    CONTEXT: process.env.CONTEXT || 'development',
-  });
+	eleventyConfig.addWatchTarget('public/'); // Watch /assets for changes
+	eleventyConfig.addPassthroughCopy({ public: '/' });
 
-  return {
-    dir: {
-      input: 'src',
-      includes: '_includes',
-      data: '_data',
-      output: '_site',
-    },
-    markdownTemplateEngine: 'njk',
-    htmlTemplateEngine: 'njk',
-    templateFormats: ['njk', 'md', 'html'],
-  };
+	// Pass environment variables to templates
+	eleventyConfig.addGlobalData('env', {
+		CONTEXT: process.env.CONTEXT || 'development',
+	});
+
+	return {
+		dir: {
+			input: 'src',
+			includes: '_includes',
+			data: '_data',
+			output: '_site',
+		},
+		markdownTemplateEngine: 'njk',
+		htmlTemplateEngine: 'njk',
+		templateFormats: ['njk', 'md', 'html'],
+	};
 }
